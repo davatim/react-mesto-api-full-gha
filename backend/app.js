53ea1324d89a63f8ec1fde09c32c0d4fd2dc1818
David Timofeev <dava.tim@yandex.ru>
2023-08-13 12:17:17 +0300

    search bag++

--- backend/app.js
+++ backend/app.js
@@ -25,9 +25,6 @@ app.use(express.json());
 app.use(cookies())
 
 app.use(helmet());
-// app.use(cors({ origin: "http://localhost:3000",
-// credentials: true,
-// }));
 app.use(router);
 
 app.use('/', (_req, res) => {

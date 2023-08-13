04dd8faeab3cf6532738493681b09cbfbd4de730
David Timofeev <dava.tim@yandex.ru>
2023-08-13 13:41:52 +0300

    search cors

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -119,7 +119,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://api.davatim.nomoreparties.co",
   headers: {
     "content-type": "application/json",
   },
--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://davatim.nomoreparties.co";
 
 export function register(password, email) {
   return fetch(`${BASE_URL}/signup`, {

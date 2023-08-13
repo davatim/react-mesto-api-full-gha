344a023e3642100551c652f1ffd065afb1b4d409
David Timofeev <dava.tim@yandex.ru>
2023-08-13 19:58:59 +0300

    Check

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -118,8 +118,8 @@ class Api {
 }
 
 const api = new Api({
-  address: "https://api.davatim.nomoreparties.co",
-  // baseUrl: "https://api.davatim.nomoreparties.co",
+  // address: "https://api.davatim.nomoreparties.co",
+  baseUrl: "https://api.davatim.nomoreparties.co",
   // baseUrl: "http://localhost:3000",
   headers: {
     "content-type": "application/json",
